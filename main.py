366d13c2bd08cd2b51ddb618aa093c4cbe26137c
Plane-walker <a990990@163.com>
2019-09-26 20:51:05 +0800

    Controler init.

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
 import pygame
 
+button = [0] * 14
+hat = [0, 0]
+axis = [0] * 6
+
+pygame.init()
+pygame.joystick.init()
+
+while True:
+    pygame.event.get()
+    joystick_count = pygame.joystick.get_count()
+
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+    name = joystick.get_name()
+
+    axes = joystick.get_numaxes()
+    for index in range(axes):
+        axis_status = joystick.get_axis(index)
+        if abs(axis[index] - axis_status) > 0.2:
+            axis[index] = axis_status
+            print("Axis {} value: {:>6.3f}".format(index, axis[index]))
+
+    buttons = joystick.get_numbuttons()
+    for index in range(buttons):
+        button_status = joystick.get_button(index)
+        if button[index] is not button_status:
+            button[index] = button_status
+            print("Button {:>2} value: {}".format(index, button[index]))
+
+    #hats = joystick.get_numhats()
+    #for index in range(hats):
+            #hat = joystick.get_hat(index)
+            #print("Hat {} value: {}".format(i, str(hat)))
