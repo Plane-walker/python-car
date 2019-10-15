db4aade75e408b6380b60b77fbcbd6f5d024afda
Plane-walker <a990990@163.com>
2019-10-15 23:05:48 +0800

    Use picamera.

--- apps/camera/camera.py
+++ apps/camera/camera.py
@@ -1,12 +1,20 @@
 import cv2
+from picamera import PiCamera
+from picamera.array import PiRGBArray
 import os
 
 def video_stream():
-    capture = cv2.VideoCapture(0)
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 32
+    rawCapture = PiRGBArray(camera, size=(640, 480))
+    # capture = cv2.VideoCapture(0)
     dir_name, file_name = os.path.split(os.path.abspath(__file__))
     face_xml = cv2.CascadeClassifier(os.path.join(dir_name, "haarcascade_frontalface_alt.xml"))
-    while capture.isOpened():
-        f, img = capture.read()
+    # while capture.isOpened():
+    #     f, img = capture.read()
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        img = frame.array
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         face = face_xml.detectMultiScale(gray, 1.1, 5)
 
@@ -17,4 +25,4 @@ def video_stream():
             cv2.destroyAllWindows()
             break
 
-    capture.release()
+    # capture.release()
