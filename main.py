5eb6986ad0dcccd4131ecb33aaeaffb7cea7f437
Plane-walker <a990990@163.com>
2020-03-09 16:56:41 +0800

    Remove import.

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import threading
 from apps.controllers.interface import Controllers
 # from apps.controllers.keyboard import keyboard_control
 # from apps.camera.color import color_detective
-from apps.motor.motor import init_motor
 
 
 def main():
     controller = Controllers()
-    init_motor()
     # video_thread = threading.Thread(target=video_stream)
     # video_thread.setDaemon(True)
     # video_thread.start()
